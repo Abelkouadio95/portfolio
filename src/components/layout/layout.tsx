e8d99e7ae78e8f226ea119ceed16a5be98245c6f
Abelkouadio95 <abelkouadio195@gmail.com>
2025-08-24 00:41:23 +0100

    own layout

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -7,7 +7,7 @@ type LayoutProps = {
 
 const Layout: React.FC<LayoutProps> = ({ children, className = "" }) => {
   return (
-    <div className={`w-full h-full inline-block z-0 bg-light px-32 ${className}`}>
+    <div className={`w-full h-full inline-block z-0 bg-light px-30 ${className}`}>
       {children}
     </div>
   );
