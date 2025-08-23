aff5a33c71433bf84e33eb6fe661cc4c96e8e3c7
Abelkouadio95 <abelkouadio195@gmail.com>
2025-08-24 00:39:45 +0100

    nav bar

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -5,6 +5,8 @@ import { usePathname } from "next/navigation";
 import React from "react";
 import { GithubIcon, LinkedInIcon } from "../icons";
 import { motion } from "framer-motion";
+import LanguageSwitcher from "../LanguageSwitcher";
+import { useTranslations } from 'next-intl';
 
 type CustomLinkProps = {
   href: string;
@@ -14,15 +16,16 @@ type CustomLinkProps = {
 
 const CustomLink: React.FC<CustomLinkProps> = ({ href, title, className = "" }) => {
   const pathname = usePathname();
+  const locale = pathname.split('/')[1] || 'fr';
 
   return (
-    <Link href={href} className={`${className} relative group`}>
+    <Link href={`/${locale}${href}`} className={`${className} relative group`}>
       {title}
       <span
         className={`
           absolute left-0 -bottom-0.5 h-[2px] bg-black
           transition-all duration-300 ease-out
-          ${pathname === href ? "w-full" : "w-0"}
+          ${pathname === `/${locale}${href}` ? "w-full" : "w-0"}
           group-hover:w-full
         `}
       />
@@ -31,24 +34,29 @@ const CustomLink: React.FC<CustomLinkProps> = ({ href, title, className = "" }) 
 };
 
 export function Navbar() {
+  const t = useTranslations('nav');
+  
   return (
     <header className="fixed bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60  w-full px-32 py-8 pb-2 font-medium flex items-center justify-between z-50">
       <nav className="flex gap-10">
-        <CustomLink href="/" title="Accueil" />
-        <CustomLink href="/about" title="A propos" />
-        <CustomLink href="/projects" title="Projets" />
-        <CustomLink href="/articles" title="Articles" />
+        <CustomLink href="/" title={t('home')} />
+        <CustomLink href="/about" title={t('about')} />
+        <CustomLink href="/projects" title={t('projects')} />
+        <CustomLink href="/articles" title={t('articles')} />
       </nav>
 
       <nav className="flex items-center justify-center flex-wrap">
         <motion.a href="/"
-        className="w-7 mr-3"
+        className="w-5 mr-3"
         whileHover={{y:-2}}
         whileTap={{scale:0.9}}> <LinkedInIcon/></motion.a>
         <motion.a href="/"
-        className="w-8 ml-3"
+        className="w-6 ml-3"
         whileHover={{y:-2}}
         whileTap={{scale:0.9}}><GithubIcon/></motion.a>
+        <div className="ml-6">
+          <LanguageSwitcher />
+        </div>
       </nav>
     </header>
   );
