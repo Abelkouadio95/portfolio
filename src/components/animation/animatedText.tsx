554e0bf0a57f77404e01383baf6a0aad8553d743
Abelkouadio95 <abelkouadio195@gmail.com>
2025-08-24 19:45:09 +0100

    animation

--- src/components/animation/animatedText.tsx
+++ src/components/animation/animatedText.tsx
@@ -39,7 +39,7 @@ const AnimatedText: React.FC<AnimatedProps> = ({text, className=""}) => {
     return(
         <div className="w-full mx-auto py-2 flex items-center justify-center text-center overflow-hidden">
             <motion.h1 
-            className={` inline-block w-full text-dark font-bold capitalize text-8xl ${className} `} 
+            className={` inline-block w-full text-dark font-bold capitalize ${className} `} 
             variants ={quote}
             initial="initial"
             animate="animate"
