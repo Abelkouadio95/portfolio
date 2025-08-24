8d2ba1d88c4ccb45263774fb6978dc7b194effa1
Abelkouadio95 <abelkouadio195@gmail.com>
2025-08-24 19:42:07 +0100

    home page

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,4 +1,3 @@
-import Layout from "@/components/layout/layout";
 import Image from "next/image";
 import image2 from "../../public/images/profiles/image3.jpg"
 import AnimatedText from "@/components/animation/animatedText";
@@ -6,12 +5,16 @@ import Link from "next/link";
 import { FaDownload } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import AnimatedSection from "@/components/animation/animatedSection";
+import Layout from "./layout/layout";
+import { useTranslations } from "next-intl";
+
+
 
 export default function HomePage() {
+  const t = useTranslations('home');
     return(
-        <main className="'flex items-center text-dark w-full min-h-screen ">
       <Layout>
-        <div className="flex items-center justify-between w-full pt-16 ">
+        <div className="flex items-center justify-between w-full">
           <div className=" pt-5 w-1/2 mr-15 relative h-[550px]">
             <Image src={image2} alt="Data IA" className="w-full h-full object-cover"></Image>
             <div className="absolute bottom-0 left-0 w-full h-[20px] bg-gradient-to-t from-white to-transparent"></div>
@@ -21,26 +24,26 @@ export default function HomePage() {
           </div>
 
           <div className=" pt-3 w-1/2 flex flex-col items-center self-center">
-            <AnimatedText text="Là où les chiffres racontent Le passé, je crée le futur." className="!text-5xl !text-left">
+            <AnimatedText text={t('title')} className="text-5xl text-left">
             </AnimatedText>
             <AnimatedSection>
-              <p className="my-4 text-base font-medium">Passionné par la donnée, j’allie l’analyse descriptive d’un Data Analyst et la puissance prédictive d’un Data Scientist. J’explore, nettoie et interprète les données pour en extraire des insights, puis je conçois des modèles de machine learning capables de transformer ces informations en solutions concrès</p>
-              <p className="font-semibold text-red-500 mb-4">Mon objectif : aider les entreprises à comprendre leur présent et anticiper leur futur.</p>
+              <h2 className="text-xl font-bold mb-2">{t('subtitle')}</h2>
+              <p className="text-base font-medium">{t('description')}</p>
+              <p className="font-semibold text-red-500 mb-4 text-base">{t('goal')}</p>
             </AnimatedSection>
             
             <div className="flex items-center self-start mt-2 gap-5">
-              <Link href="/Cv/Cv_Data Scientist_Coface_AbelKouadio.pdf" target={"_blank"} 
-              className="flex items-center bg-black text-white p-2 px-5 rounded-lg font-semibold hover:bg-white border-2 hover:text-black gap-4">
-                <FaDownload/>Telecharger mon CV
+              <Link href={t('cvLink')} target={"_blank"} 
+              className="flex items-center bg-black text-white p-2 px-5 rounded-lg font-semibold hover:bg-white border-2 hover:text-black gap-4 text-sm">
+                <FaDownload/> {t('cv')}
               </Link>
-              <Link href="abelkouadio195@gmail.com" target={"_blank"} className="flex items-center bg-black text-white p-2 px-5 rounded-lg font-semibold hover:bg-white border-2 hover:text-black gap-4"><MdEmail/> Contactez moi</Link>
+              <a href="mailto:abelkouadio195@gmail.com" target={"_blank"} className="flex items-center bg-black text-white text-sm p-2 px-5 rounded-lg font-semibold hover:bg-white border-2 hover:text-black gap-4"><MdEmail/>{t('contact')} </a>
             </div>
           </div>  
-
-          
-          
         </div>
       </Layout>
-    </main>
+        
+
+
     )
 }
