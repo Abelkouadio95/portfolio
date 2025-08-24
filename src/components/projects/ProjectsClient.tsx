ca2af001e3d6ecde9bc94e8ddeb37cd27cf5dea0
Abelkouadio95 <abelkouadio195@gmail.com>
2025-08-24 19:44:34 +0100

    projects

--- src/components/projects/ProjectsClient.tsx
+++ src/components/projects/ProjectsClient.tsx
@@ -4,9 +4,9 @@ import AnimatedSection from "@/components/animation/animatedSection";
 import CardContainer from "@/components/layout/CardContainer";
 import ProjectCard from "@/components/projects/ProjectCard";
 import ProjectModal from "@/components/projects/ProjectModal";
-import { projects } from "@/data/projects";
 import type { Project } from "@/types/portfolio";
 import { FaFolderOpen } from "react-icons/fa";
+import { useTranslations } from "next-intl";
 
 export default function ProjectsClient() {
   const [selected, setSelected] = useState<Project | null>(null);
@@ -18,24 +18,26 @@ export default function ProjectsClient() {
   };
 
   const handleClose = () => setOpen(false);
+  const t = useTranslations('projects');
+  const projectsData= t.raw('projectsData') as Project[];
 
   return (
     <div className="pt-24 md:pt-28">
       <AnimatedSection>
         <CardContainer
-          title="Projets"
+          title={t('projectsTitle')}
           icon={<FaFolderOpen size={28} className="text-gray-700" />}
           className="mb-8"
         >
           <p className="text-gray-700 dark:text-gray-300">
-            Ici, vous trouverez une sélection de mes projets personnels et professionnels: data science, IA, web full‑stack et outils. Cliquez sur « Voir plus » pour découvrir les détails techniques, les fonctionnalités et les choix d'implémentation.
+            {t('projectsContent')}
           </p>
         </CardContainer>
       </AnimatedSection>
 
       <AnimatedSection>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          {projects.map((p) => (
+          {projectsData.map((p) => (
             <ProjectCard key={p.id} project={p} onVoirPlus={handleVoirPlus} />
           ))}
         </div>
