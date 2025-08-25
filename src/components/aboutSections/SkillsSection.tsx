7aa67a957652dace78de36f3439408d1d7ba826f
Abelkouadio95 <abelkouadio195@gmail.com>
2025-08-25 18:09:31 +0100

    skills section

--- src/components/aboutSections/SkillsSection.tsx
+++ src/components/aboutSections/SkillsSection.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { motion } from "framer-motion";
-import type { Skill } from "@/types/portfolio";
+import type { Skill } from "@/types/portfolioTypes";
 import { useTranslations } from "next-intl";
 
 interface SkillsSectionProps {
