f1459866303fd3a5d04b95181fe221d5ac2f05c2
Abelkouadio95 <abelkouadio195@gmail.com>
2025-08-26 23:20:01 +0100

    Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 
-export const metadata: Metadata = {
-  title: 'Portfolio - Abel Kouadio',
-  description: 'Data Scientist & Full Stack Developer',
-}
+
 
 export default function RootLayout({
   children,
