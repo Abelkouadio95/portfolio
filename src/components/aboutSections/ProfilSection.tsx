46074a246737f2d6c80ad5a927431e4d764df28d
Abelkouadio95 <abelkouadio195@gmail.com>
2026-03-07 00:24:34 +0000

    profil section

--- src/components/aboutSections/ProfilSection.tsx
+++ src/components/aboutSections/ProfilSection.tsx
@@ -1,12 +1,19 @@
 "use client";
 
 interface Profil {
-  profil: string;
+  profil: string[];
 }
 
 
 export default function ProfilSection({ profil }: Profil) {
     return(
-        <div> {profil} </div>
+        <div>
+            {profil.map((pr,i) => (
+                <p key={i} className="text-gray-600 dark:text-gray-300 mb-1">
+                    {pr}
+                </p>
+                ))
+            }
+        </div>
     )
 }
